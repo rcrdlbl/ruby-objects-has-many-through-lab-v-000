868a662925f82d5a7a46527b4c128e034398e19d
Przemyslaw Dabrowski <bluh@outlook.com>
2018-08-12 01:27:49 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,4 +4,13 @@ class Patient
   def initialize(name)
     @name = name
   end
+
+  def new_appointment(doctor, date)
+    Appointment.new(self, doctor, date)
+
+  end
+
+  def appointments
+
+  end
 end
